eb73f527330837439d848b2a95794ac60c152e1b
Ramanarayana <gunjirama1999@gmail.com>
2023-07-31 04:47:21 +0530

    subnets creating

--- VPC/VPC.tf
+++ VPC/VPC.tf
@@ -6,3 +6,19 @@ resource "aws_vpc" "main" {
     Name = "automated-vpc"
   }
 }
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet-automated-vpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private-subnet-automated-vpc"
+  }
+}
