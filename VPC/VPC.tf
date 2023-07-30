5c89b3caeea34276484417460bb04c3b5c1a8c3f
Ramanarayana <gunjirama1999@gmail.com>
2023-07-31 05:08:33 +0530

    natgateway, route table create

--- VPC/VPC.tf
+++ VPC/VPC.tf
@@ -40,6 +40,35 @@ resource "aws_route_table" "public-rt" {
 
 
   tags = {
-    Name = "public-rt"
+    Name = "automated-public-rt"
+  }
+}
+resource "aws_eip" "auto-eip" {
+
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.auto-eip.id
+  subnet_id     = aws_subnet.private.id
+
+  tags = {
+    Name = "automated-NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+
+
+  tags = {
+    Name = "automated-private-rt"
   }
 }
