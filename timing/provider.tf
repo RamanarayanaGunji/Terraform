20fe0a435a99c9c78975f490f9f39727b235d37e
Ramanarayana <gunjirama1999@gmail.com>
2023-08-03 12:38:51 +0530

    timing created2

--- timing/provider.tf
+++ timing/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
     backend "s3" {
-      bucket = "timing-remote-state"
+      bucket = "timing-local"
       key    = "timing"
       region = "us-east-1"
       dynamodb_table ="timing-lock"
